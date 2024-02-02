1af8b7a8a7229929a43f3d72a631572014967422
KJK <kody.j.king@gmail.com>
2024-02-02 04:04:29 -0800

    tweaked scripts

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,8 +1,13 @@
 # Delete /bin and /obj folders.
 Get-ChildItem -Path . -Include bin,obj -Recurse | Remove-Item -Recurse -Force
-# Delete .sln and .vcxproj files.
-Get-ChildItem -Path . -Include *.sln,*.vcxproj -Recurse | Remove-Item -Force
+
+# Delete .sln and .vcxproj files, but ignore /vendor.
+Get-ChildItem -Path . -Include *.sln,*.vcxproj -Recurse | Where-Object { $_.DirectoryName -notlike "*\vendor*" } | Remove-Item -Force
+
 # Delete vendor/*/bin and vendor/*/obj folders.
 Get-ChildItem -Path vendor -Directory | ForEach-Object {
     Get-ChildItem -Path $_.FullName -Include bin,obj -Recurse | Remove-Item -Recurse -Force
 }
+
+# Delete imgui.ini.
+Remove-Item -Path imgui.ini -Force
--- scripts/watch.ps1
+++ scripts/watch.ps1
@@ -1,67 +1,87 @@
+# See https://powershell.one/tricks/filesystem/filesystemwatcher
+
 param(
     [string]$watchPath,
     [string]$command
 )
 
-Write-Host "Watching $watchPath for changes"
+$global:process = $null
+$global:needsRestart = $false
 
-# Define global copy of command
-$global:command = $command
-$global:process = Start-Process powershell -NoNewWindow -PassThru -ArgumentList "-Command", $global:command
+function Run-Command {
+    $global:process = Start-Process powershell -NoNewWindow -PassThru -ArgumentList "-Command", $command
+}
+
+function Kill-Tree {
+    Param([int]$ppid)
+    Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $ppid } | ForEach-Object { Kill-Tree $_.ProcessId }
+    Stop-Process -Id $ppid -Force
+}
+
+function On-Exit {
+    Write-Host "Unwatching file system." -ForegroundColor Blue
+    $watcher.EnableRaisingEvents = $false
+    $handlers | ForEach-Object { Unregister-Event -SourceIdentifier $_.Name }
+    $handlers | Remove-Job
+    $watcher.Dispose()
+
+    if ($global:process) {
+        Write-Host "Cleaning up process." -ForegroundColor Blue
+        Kill-Tree $global:process.Id
+    } else {
+        Write-Host "No process to clean up." -ForegroundColor Blue
+    }
+}
 
 try {
+    Write-Host "Watching $watchPath for changes" -ForegroundColor Green
+
+    Run-Command
+
     $watcher = New-Object System.IO.FileSystemWatcher
     $watcher.Path = $watchPath
     $watcher.IncludeSubdirectories = $true
     $watcher.EnableRaisingEvents = $true
 
     $action = {
+        if ($needsRestart) { return }
         $path = $Event.SourceEventArgs.FullPath
         $changeType = $Event.SourceEventArgs.ChangeType
-        Write-Host "File $path $changeType"
-
-        # Clean up the process
-        if ($global:process) {
-            # Have to define this twice due to scoping issues :/
-            function Kill-Tree { # Source https://stackoverflow.com/a/55942155
-                Param([int]$ppid)
-                Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $ppid } | ForEach-Object { Kill-Tree $_.ProcessId }
-                Stop-Process -Id $ppid -Force
-            }
-            Kill-Tree $global:process.Id
-        }
-
-        $global:process = Start-Process powershell -NoNewWindow -PassThru -ArgumentList "-Command", $global:command
+        $changeType = $changeType.ToString().ToLower()
+        Write-Host ""
+        Write-Host "File $path $changeType." -ForegroundColor Yellow
+        Write-Host ""
+        $global:needsRestart = $true
     }
 
-    Register-ObjectEvent -InputObject $watcher -EventName "Created" -Action $action
-    Register-ObjectEvent -InputObject $watcher -EventName "Changed" -Action $action
-    Register-ObjectEvent -InputObject $watcher -EventName "Deleted" -Action $action
-    Register-ObjectEvent -InputObject $watcher -EventName "Renamed" -Action $action
+    $handlers = . {
+        Register-ObjectEvent -InputObject $watcher -EventName "Created" -Action $action
+        Register-ObjectEvent -InputObject $watcher -EventName "Changed" -Action $action
+        Register-ObjectEvent -InputObject $watcher -EventName "Deleted" -Action $action
+        Register-ObjectEvent -InputObject $watcher -EventName "Renamed" -Action $action
+    }
 
-    # See https://powershell.one/tricks/filesystem/filesystemwatcher#implementation
     do {
-        # Wait-Event waits for a second and stays responsive to events
-        Wait-Event -Timeout 1
+        Start-Sleep -Milliseconds 20
+
+        # Press R to manually restart the process
+        if ([Console]::KeyAvailable) {
+            $key = [Console]::ReadKey($true)
+            if ($key.Key -eq "R") {
+                $global:needsRestart = $true
+            }
+        }
+
+        if ($global:needsRestart) {
+            $global:needsRestart = $false
+            if ($global:process) {
+                Kill-Tree $global:process.Id
+            }
+            Run-Command
+        }
+
     } while ($true)
 
 } finally {
-
-    # Clean up the watcher
-    Write-Host "Watch stopped."
-    $watcher.Dispose()
-
-    # Clean up the process
-    if ($global:process) {
-        Write-Host "Cleaning up process."
-        function Kill-Tree {
-            Param([int]$ppid)
-            Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $ppid } | ForEach-Object { Kill-Tree $_.ProcessId }
-            Stop-Process -Id $ppid -Force
-        }
-        Kill-Tree $global:process.Id
-    } else {
-        Write-Host "No process to clean up."
-    }
-
+    On-Exit
 }
